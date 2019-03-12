3aa1ee01950373dcfc9462c88ab6c7b443ec2c66
daniel 'kirplink' reynolds <=>
2019-03-11 21:46:16 -0500

    landing page changes

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,10 +1,55 @@
-import React from "react";
-import '../Navbar/Navbar.css'
+import React, { Component } from "react";
+import "../Navbar/Navbar.css";
+import {
+	MDBNavbar,
+	MDBNavbarBrand,
+	MDBNavbarNav,
+	MDBNavItem,
+	MDBNavLink,
+	MDBNavbarToggler,
+	MDBCollapse
+} from "mdbreact";
 
-function Navbar () {
-    
-    return <div className="nav"><a href="/">Hidden Locales</a><div><a href="/profile">Profile</a><a href="/trips">Trips</a><a href="/locale">Suggest Locale</a></div></div>
+class Navbar extends Component {
+	state = {
+		isOpen: false
+	};
 
+	toggleCollapse = () => {
+		this.setState({ isOpen: !this.state.isOpen });
+	};
+
+	render() {
+		return (
+			<MDBNavbar className="burntOrange" dark expand="md">
+				<MDBNavbarBrand>
+					<strong className="white-text">Hidden Locales</strong>
+				</MDBNavbarBrand>
+				<MDBNavbarToggler onClick={this.toggleCollapse} />
+				<MDBCollapse id="navbarCollapse3" isOpen={this.state.isOpen} navbar>
+					<MDBNavbarNav right>
+						<MDBNavItem active={this.props.profile}>
+							<MDBNavLink to="#!">Profile</MDBNavLink>
+						</MDBNavItem>
+						<MDBNavItem active={this.props.trips}>
+							<MDBNavLink to="#!">Trips</MDBNavLink>
+						</MDBNavItem>
+						<MDBNavItem active={this.props.locale}>
+							<MDBNavLink to="#!">Suggest Locale</MDBNavLink>
+						</MDBNavItem>
+					</MDBNavbarNav>
+				</MDBCollapse>
+			</MDBNavbar>
+			// <div className="nav">
+			// 	<a href="/">Hidden Locales</a>
+			// 	<div>
+			// 		<a href="/profile">Profile</a>
+			// 		<a href="/trips">Trips</a>
+			// 		<a href="/locale">Suggest Locale</a>
+			// 	</div>
+			// </div>
+		);
+	}
 }
 
-export default Navbar
+export default Navbar;
